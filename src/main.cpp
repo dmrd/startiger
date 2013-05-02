9b331a3b73760dbc97abc9378117a4b5dc4a44a6
David Dohan <dmrdohan@gmail.com>
2013-05-02 14:48:08 -0400

    Basic makefile, glut libraries, empty main

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+/**************************************
+ * StarTiger main
+ * COS426 - Princeton University
+ * Team:
+ * Mark Benjamin
+ * David Bieber
+ * David Dohan
+ * Nikhilesh Sigatapu
+ ***************************************/
+//Import GLUT
+#if defined(_WIN32) || defined(__CYGWIN__)
+# ifndef USE_OPENGL32
+#  define USE_OPENGL32
+# endif
+# ifndef NOMINMAX
+#  define NOMINMAX
+# endif
+# include <windows.h>
+# include <GL/glut.h>
+#elif defined(__APPLE__)
+# include <GLUT/glut.h>
+#else 
+# include <GL/glut.h>
+#endif
+
+
+int main(int argc, char **argv) {
+
+
+}
